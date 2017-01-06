d0c0d2d10ecd6659854deeba410efcd1f17fb560
Nilotpal Roychowdhury <nilotpal.roy05@gmail.com>
2017-01-07 03:19:55 +0530

    Added an intent

--- app/src/main/java/com/nilotpal/groove/MainActivity.java
+++ app/src/main/java/com/nilotpal/groove/MainActivity.java
@@ -1,5 +1,8 @@
 package com.nilotpal.groove;
 
+import android.content.Intent;
+import android.view.View;
+
 import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -11,5 +14,29 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Find the view containing the Now playing category
+        TextView now_playing = (TextView) findViewById(R.id.content1);
+
+        // Set a click listener on that View
+        now_playing.setOnClickListener(new View.OnClickListener() {
+            //Overriding the onClick method defined in the View.class
+            @Override
+            public void onClick(View view) {
+                //Create an explicit intent to open another window
+                Intent nowPlayingIntent = new Intent(MainActivity.this, NowPlaying.class);
+                startActivity(nowPlayingIntent);
+            }
+        });
+
+
+
+
+
+
+
+
     }
 }
+
+
