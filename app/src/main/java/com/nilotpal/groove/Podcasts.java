cefd28164ff6cefd71c42f072032b578d10f5fa0
Nilotpal Roychowdhury <nilotpal.roy05@gmail.com>
2017-01-07 03:45:06 +0530

    Added all the activities

---
+++ app/src/main/java/com/nilotpal/groove/Podcasts.java
@@ -0,0 +1,13 @@
+package com.nilotpal.groove;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Podcasts extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_podcasts);
+    }
+}
